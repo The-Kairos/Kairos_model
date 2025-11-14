b82318b1f921f143e54660dee9889a3eb9ce3639
joey-en <joyannpdelacruz@gmail.com>
2025-11-14 13:55:03 +0300

    Added BLIP captioning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.scene_cutting import get_scene_list
 from src.frame_sampling import sample_frames
-from src.frame_captioning import caption_frames
+from src.frame_captioning_blip import caption_frames
 
 test_video = r'Videos\SpongeBob SquarePants - Writing Essay - Some of These - Meme Source.mp4'
 scenes = get_scene_list(test_video)
@@ -17,5 +17,14 @@ scenes_with_frames = sample_frames(
     input_video_path=test_video,
     scenes=scenes,
     num_frames=4,
+    output_dir="./output/frames",
 )
 
+captioned_scenes = caption_frames(
+    scenes=scenes_with_frames,
+    max_length=30,
+    num_beams=4,
+    do_sample=False,
+    debug=True,
+    prompt="a video frame of"
+)
