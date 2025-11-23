c1cd7071976c89c3a7cd50fa5e926887dea52af8
joey-en <joyannpdelacruz@gmail.com>
2025-11-23 19:48:35 +0300

    Ran complete pipeline with audio

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from src.debug_utils import *
 from src.log_utils import *
 
 test_videos = {
-    "sponge": r"Videos\SpongeBob SquarePants - Writing Essay - Some of These - Meme Source.mp4",
-    "malala": r"Videos\Watch Malala Yousafzai's Nobel Peace Prize acceptance speech.mp4",
-    "tire": r"Videos\Cartastrophe.mp4",
-    "spain": r"Videos\Spain Vlog.mp4",
+    "sponge_pyscene_blip_yolo_ASR_AST_GeminiPro25": r"Videos\SpongeBob SquarePants - Writing Essay - Some of These - Meme Source.mp4",
+    "malala_pyscene_blip_yolo_ASR_AST_GeminiPro25": r"Videos\Watch Malala Yousafzai's Nobel Peace Prize acceptance speech.mp4",
+    "tire_pyscene_blip_yolo_ASR_AST_GeminiPro25": r"Videos\Cartastrophe.mp4",
+    "spain_pyscene_blip_yolo_ASR_AST_GeminiPro25": r"Videos\Spain Vlog.mp4",
 }
 OUTPUT_DIR = "spain"
 
