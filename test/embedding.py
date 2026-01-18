0cb32950d15f996b085e5d972810f8133de0ae0c
joey-en <joyannpdelacruz@gmail.com>
2026-01-18 17:04:09 +0300

    Addded embedding based on ai.google.dev/gemini-api/docs

--- test/embedding.py
+++ test/embedding.py
@@ -1,4 +1,12 @@
 import json
+import os
+from dotenv import load_dotenv
+from google import genai
+
+# https://ai.google.dev/gemini-api/docs/embeddings#generate-embeddings
+load_dotenv(".env")
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(vertexai=True, api_key=api_key) # put "vertexai=True" if you're using Dr. Oussama's key
 
 def format_embedding_text(scenes: list):
     embedding_texts = []
@@ -22,9 +30,37 @@ def format_embedding_text(scenes: list):
         
     return embedding_texts
 
-log_path= r"logs\car_pyscene_blip_yolo_ASR_AST_GeminiPro25_20251123_180938.json"
-with open(log_path, "r", encoding="utf-8") as f:
-    logs = json.load(f)
-scenes = format_embedding_text(logs.get("scenes"))
+def embed_scenes(scenes: list):
+    # https://ai.google.dev/gemini-api/docs/embeddings#generate-embeddings
+    result = client.models.embed_content(
+        model="gemini-embedding-001",
+        contents=scenes
+    )
+    return [embedding for embedding in result.embeddings]
 
-print(scenes)
+def embed_question(question: str):
+    # https://ai.google.dev/gemini-api/docs/embeddings#generate-embeddings
+    result = client.models.embed_content(
+        model="gemini-embedding-001",
+        contents=question
+    )
+    return result.embeddings
+
+# REAL TESTING
+# log_path= r"logs\car_pyscene_blip_yolo_ASR_AST_GeminiPro25_20251123_180938.json"
+# with open(log_path, "r", encoding="utf-8") as f:
+#     logs = json.load(f)
+# scenes = format_embedding_text(logs.get("scenes"))
+# question = input("Give questions about cartastrophy: ")
+# --------------------------------------------------------
+scenes = [
+    "The cat sat on the mat.",
+    "A dog ran in the park.",
+    "The weather is sunny today.",
+    "The cat is black and white."
+]
+question = "Is it sunny today"
+
+
+scene_embeddings  = embed_scenes(scenes)
+question_embedding = embed_question(question)
