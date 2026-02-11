10e86057499ab33d02616edb7f25bfb5b3c67478
joey-en <joyannpdelacruz@gmail.com>
2026-02-11 23:47:20 +0300

    Removed old rag

--- rag/retrieve.py
+++
@@ -1,7 +0,0 @@
-from embedding import embed_query
-from config import TOP_K
-
-def retrieve_context(question, store):
-    q_vec = embed_query(question)
-    results = store.search(q_vec, k=TOP_K)
-    return results
